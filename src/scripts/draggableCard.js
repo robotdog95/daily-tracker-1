08671961ede4bda7478f67c435a8d6c9d3553661
sevenist <129137530+sevenist@user.noreply.github.com>
2026-02-21 09:08:40 +0100

    ajouts de composants

--- src/scripts/draggableCard.js
+++ src/scripts/draggableCard.js
@@ -13,9 +13,41 @@ export class Draggable extends AppComponent {
   }
 
   async connectedCallback() {
-    console.error("setting stuff")
+    console.error("setting stuff");
     this.innerHTML = await this.getHTML();
     this.setAttribute("draggable", true);
+    this.addEventListener("dragstart", (ev) => {
+      // On ajoute l'identifiant de l'élément cible à l'objet de transfert
+      ev.dataTransfer.setData("text/plain", ev.target.innerText);
+      console.log(ev)
+    });
+    this.addEventListener("dragend", (ev) => {
+      console.log(ev)
+    });
+    function handleDragStart(e) {
+    this.style.opacity = '0.4';
+  }
+
+  function handleDragEnd(e) {
+    this.style.opacity = '1';
+
+    items.forEach(function (item) {
+      item.classList.remove('over');
+    });
+  }
+
+  function handleDragOver(e) {
+    e.preventDefault();
+    return false;
+  }
+
+  function handleDragEnter(e) {
+    this.classList.add('over');
+  }
+
+  function handleDragLeave(e) {
+    this.classList.remove('over');
+  }
   }
 }
 
